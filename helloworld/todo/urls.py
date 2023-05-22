1a987d142e288c62e41c123e58a4fb8665ba7fcb
evgeni-semenov <evgeni.semenov@gmail.com>
2023-05-22 13:53:43 +0300

    Removed unnecessary stuff

--- helloworld/todo/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-		path('about', AboutView.as_view()),
-]
--- helloworld/todo/views.py
+++
@@ -1,4 +0,0 @@
-from django.views.generic import TemplateView
-
-class AboutView(TemplateView):
-		template_name = "todo/about.html"
