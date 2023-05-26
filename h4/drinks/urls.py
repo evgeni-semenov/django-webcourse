dd85b5e725dbdf68bc85bbc978ff82096e5fff52
evgeni-semenov <evgeni.semenov@gmail.com>
2023-05-26 15:11:30 +0300

    Login and registration forms added

--- h4/drinks/urls.py
+++ h4/drinks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
 
 urlpatterns = [
@@ -6,5 +7,11 @@ urlpatterns = [
     path('drinks/', DrinkListView.as_view()),
     path('drinks/<int:pk>/edit', DrinkUpdateView.as_view()),
     path('drinks/new/', DrinkCreateView.as_view()),
+
     path('types', DrinkTypeListView.as_view()),
+    
+    path('accounts/login/', LoginView.as_view(next_page="/")),
+    path('logout', LogoutView.as_view(next_page="/")),
+
+    path('register', RegisterView.as_view()),
 ]
--- h4/drinks/views.py
+++ h4/drinks/views.py
@@ -1,22 +1,31 @@
 from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import *
 
 class DrinkListView(ListView):
     model = Drink
 
-class DrinkUpdateView(UpdateView):
+class DrinkUpdateView(LoginRequiredMixin, UpdateView):
     model = Drink
     fields = ["name", "type_name", "alcohol_content"]
     success_url = "/"
 
-class DrinkCreateView(CreateView):
+class DrinkCreateView(LoginRequiredMixin, CreateView):
     model = Drink
     fields = ["name", "type_name", "alcohol_content"]
     success_url = "/"
 
-class DrinkDeleteView(DeleteView):
+class DrinkDeleteView(LoginRequiredMixin, DeleteView):
     model = Drink
     success_url = "/"
 
 class DrinkTypeListView(ListView):
     model = DrinkType
+
+class RegisterView(SuccessMessageMixin, CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/register.html"
+    success_url = "/accounts/login"
+    success_message = "Registration accepted. You can now log in."
